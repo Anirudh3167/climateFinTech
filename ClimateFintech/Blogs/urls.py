7e9945e8c0bda06506331952ad5922e941a481e6
Anirudh <anirudhmukkamala@gamil.com>
2022-12-10 16:41:00 +0530

    Blogs page is partially done

--- ClimateFintech/Blogs/urls.py
+++ ClimateFintech/Blogs/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('',Home,name='home'),
     path('create-post/',CreateBlog,name='create-post'),
     path('blog-display/<str:search>',BlogDisplay,name='blog-display'),
+    path('user-blogs/',UserBlogs,name='User-Blogs'),
+    path('delete-post/<str:id>',DeletePost,name='DeletePost'),
 ]
--- ClimateFintech/Blogs/views.py
+++ ClimateFintech/Blogs/views.py
@@ -3,6 +3,8 @@ from Backend.models import Blogs
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from django.contrib.auth import get_user_model
+from django.forms.models import model_to_dict
+
 # Create your views here.
 
 # assigning User as our User model i.e. as User_details
@@ -19,7 +21,7 @@ def CreateBlog(request):
         Author = request.user
         new_blog = Blogs.objects.create(Author = Author, title = title, content = content)
         new_blog.save()
-        return redirect(reverse('blogs:blog-display',kwargs={'search':str(title)}))
+        return redirect('blogs:User-Blogs')
     return render(request,'blogs/create_blog.html')
 
 def BlogDisplay(request,search):
@@ -28,8 +30,32 @@ def BlogDisplay(request,search):
     for i in search:    search1 = search1+' '+i
     print("SEARCH1:",search1)
     result = Blogs.objects.filter(title = search1[1:]).first()
-    context = {
-        "title":result.title,
-        "content":result.content.split('\n'),
-    }
+    if result:
+        context = {
+            "title":result.title,
+            "content":result.content.split('\n'),
+        }
+    else:
+        return redirect('blogs:home')
     return render(request,'blogs/blog_display.html',context)
+
+def UserBlogs(request):
+    if request.method == 'GET':
+        user = request.user
+        user_blogs = Blogs.objects.filter(Author=user)
+        blogs = {}
+        count = 0
+        for blog in user_blogs:
+            blogs[count] = model_to_dict(user_blogs[count])
+            count = count + 1
+        # print(blogs)
+        blogging = {}
+        blogging["blogs"] = dict(blogs)
+        return render(request,'blogs/user_blogs.html',blogging)
+    elif request.method == 'POST':
+        search = request.POST['search-title']
+        return BlogDisplay(request,search)
+
+def DeletePost(request,id):
+    post = Blogs.objects.get(id = id).delete()
+    return redirect('blogs:home')
